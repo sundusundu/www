55fb230c2fe12cebc4fc23e75897e05b0f5f4438
sundusundu <sundusundu@163.com>
2014-03-29 16:18:20 -0400

    my 8th check-in

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     if (isset($_POST['submit'])) {
  
        //$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	   $dbc = mysqli_connect($_SERVER['aalio1rdc2w9hm.c6nr3pcl42so.us-west-2.rds.amazonaws.com'], $_SERVER['ebroot'], $_SERVER['11111111'], $_SERVER['ebdb'], $_SERVER['3306']);
+	   $dbc = mysqli_connect('aalio1rdc2w9hm.c6nr3pcl42so.us-west-2.rds.amazonaws.com', 'ebroot', '11111111', 'ebdb', 3306);
 
       //$user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
       //$user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
