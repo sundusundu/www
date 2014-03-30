e4b43bdde6323d56686c8a739025a6fad46b8614
sundusundu <sundusundu@163.com>
2014-03-29 21:19:32 -0400

    final f**k

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
           $_SESSION['username'] = $row['username'];
           setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
           setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
-          $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/home.php';
+          $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . 'home.php';
           header('Location: ' . $home_url);
         }
         else {
